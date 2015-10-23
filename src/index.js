f4614efae112b9685116ec7b6eca0b364267e5f4
Chris McLean <havok2905@gmail.com>
2015-10-23 11:02:08 -0400

    nuked dependencies

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import TableComponentController from './table_component_controller';
-import TableComponentViews from './table_component_views';
-import ScreenCalulator from './screen_calculator';
-
-export default {
-  TableComponentController: TableComponentController,
-  TableComponentViews:      TableComponentViews,
-  ScreenCalulator:          ScreenCalulator
-};
