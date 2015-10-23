19fce4d53f4239433c7349e2d17fb99912735ebe
Chris McLean <havok2905@gmail.com>
2015-10-23 10:41:03 -0400

    packaged

--- app/main.js
+++
@@ -1,8 +0,0 @@
-import ResponsiveTable from '../src/responsive_table';
-
-let breakPoint  = 800,
-    element = document.getElementById('better-table'),
-    table = new ResponsiveTable.TableComponentController({
-      breakPoint: breakPoint,
-      element: element
-    });
--- gulpfile.js
+++ gulpfile.js
@@ -2,26 +2,24 @@ var gulp       = require('gulp');
 var browserify = require('browserify');
 var babelify   = require('babelify');
 var source     = require('vinyl-source-stream');
-var sass       = require('gulp-sass');
-
-gulp.task('scss', function() {
-  gulp.src('./scss/main.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('.'));
-});
+var uglify     = require('gulp-uglify');
 
 gulp.task('js', function () {
-  return browserify({entries: './app/main.js', extensions: ['.js'], debug: true})
+  return browserify({entries: './src/index.js', extensions: ['.js'], debug: true})
     .transform(babelify)
     .bundle()
-    .pipe(source('bundle.js'))
+    .pipe(source('index.js'))
     .pipe(gulp.dest('.'));
 });
 
-gulp.task('watch', ['js', 'scss'], function () {
+gulp.task('compress', ['js'], function() {
+  return gulp.src('./index.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', ['compress'], function () {
   gulp.watch('./app/**/*.js', ['js']);
-  gulp.watch('./scss/**/*.scss', ['scss']);
 });
 
 gulp.task('default', ['watch']);
